e1ce3175d4a2370cc95afeec2bf456489aec2be6
cctxjj <sebastian.dietrich7@outlook.de>
2024-03-06 19:49:54 +0100

    minor changes

--- model_1_dataset_2.py
+++ model_1_dataset_2.py
@@ -2,45 +2,46 @@ import tensorflow as tf
 
 import tumor_classification
 
-
-dataset_training: tf.data.Dataset = tf.keras.utils.image_dataset_from_directory(
-    directory='dataset_2/Training',
-    seed=123,
-    image_size=(512, 512),
-)
-dataset_training = dataset_training.cache()
-dataset_training = dataset_training.prefetch(buffer_size=tf.data.AUTOTUNE)
-
 kernel_size = (3, 3)
 pool_size = (2, 2)
 epochs = 20
+
+normal_dropout_rate = 0.5
+spatial_dropout_rate = 0.5
 
 
 layers = [
     tf.keras.layers.Rescaling(1. / 255, input_shape=(512, 512, 3)),
 
+
     tf.keras.layers.Conv2D(16, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.2),
     tf.keras.layers.Conv2D(32, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.2),
+    tf.keras.layers.SpatialDropout2D(rate=spatial_dropout_rate),
     tf.keras.layers.Conv2D(64, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.1),
     tf.keras.layers.Conv2D(128, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.05),
+    tf.keras.layers.SpatialDropout2D(rate=spatial_dropout_rate),
     tf.keras.layers.Conv2D(256, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.05),
+    tf.keras.layers.Conv2D(512, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
+    tf.keras.layers.MaxPooling2D(pool_size=pool_size),
+    tf.keras.layers.Conv2D(512, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
+    tf.keras.layers.MaxPooling2D(pool_size=pool_size),
+    tf.keras.layers.SpatialDropout2D(rate=spatial_dropout_rate),
 
     tf.keras.layers.Flatten(),
 
     tf.keras.layers.Dense(units=64, activation=tf.keras.activations.relu),
     tf.keras.layers.Dense(units=128, activation=tf.keras.activations.relu),
+    tf.keras.layers.Dropout(rate=normal_dropout_rate),
     tf.keras.layers.Dense(units=256, activation=tf.keras.activations.relu),
-    tf.keras.layers.Dense(units=256, activation=tf.keras.activations.relu),
+    tf.keras.layers.Dropout(rate=normal_dropout_rate),
+    tf.keras.layers.Dense(units=512, activation=tf.keras.activations.relu),
+    tf.keras.layers.Dropout(rate=normal_dropout_rate),
+    tf.keras.layers.Dense(units=512, activation=tf.keras.activations.relu),
 
     tf.keras.layers.Dense(units=4, activation=tf.keras.activations.softmax)
 ]
@@ -53,7 +54,7 @@ model = tumor_classification.ImageRecognizer(
     layers=layers,
     model_tag="model_" + model_n + "_d_3_conv_10_ep_" + str(epochs),
     batch_size=32,
-    compile_information=[tf.keras.optimizers.Adam(), tf.keras.losses.SparseCategoricalCrossentropy(),
+    compile_information=[tf.keras.optimizers.SGD(), tf.keras.losses.SparseCategoricalCrossentropy(),
                          [tf.keras.metrics.categorical_accuracy]],
     img_size=(512, 512),
     dataset_tag="dataset_2"
