c63c3f0f727a32b6af79ba4175a2d7fdf29dbdf8
ctxj <sebastian.dietrich7@outlook.de>
2024-03-06 19:45:25 +0100

    mc

--- model_1_dataset_2.py
+++ model_1_dataset_2.py
@@ -13,7 +13,7 @@ dataset_training = dataset_training.prefetch(buffer_size=tf.data.AUTOTUNE)
 
 kernel_size = (3, 3)
 pool_size = (2, 2)
-epochs = 20
+epochs = 40
 
 
 layers = [
@@ -21,31 +21,25 @@ layers = [
 
     tf.keras.layers.Conv2D(16, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.2),
     tf.keras.layers.Conv2D(32, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.2),
     tf.keras.layers.Conv2D(64, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.1),
     tf.keras.layers.Conv2D(128, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.05),
     tf.keras.layers.Conv2D(256, kernel_size=kernel_size, activation=tf.keras.activations.relu, padding="same"),
     tf.keras.layers.MaxPooling2D(pool_size=pool_size),
-    tf.keras.layers.SpatialDropout2D(rate=0.05),
 
     tf.keras.layers.Flatten(),
 
     tf.keras.layers.Dense(units=64, activation=tf.keras.activations.relu),
     tf.keras.layers.Dense(units=128, activation=tf.keras.activations.relu),
     tf.keras.layers.Dense(units=256, activation=tf.keras.activations.relu),
-    tf.keras.layers.Dense(units=256, activation=tf.keras.activations.relu),
 
     tf.keras.layers.Dense(units=4, activation=tf.keras.activations.softmax)
 ]
 
-model_n = str(1)
+model_n = str(2)
 
 model = tumor_classification.ImageRecognizer(
     dataset_dir="dataset_2/Training",
@@ -59,6 +53,6 @@ model = tumor_classification.ImageRecognizer(
     dataset_tag="dataset_2"
 )
 
-model.train(True, epochs, True)
+model.train(False, epochs, True)
 
 model.evaluate_on_unknown_dataset("dataset_2/Testing")
